a34b8ab189736a7a3551e26fa28353b5ca4ea53d
Egor Quenum <georgequenum@yahoo.com>
2024-03-17 03:34:01 +0400

    Add import aliases

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,10 +1,11 @@
-import { ProjectInput } from './services/project-input.js';
-import { ProjectList } from './services/project-list.js';
+// aliases
+import * as Proj from './services/project-input.js';
+import { ProjectList as ProjList } from './services/project-list.js';
 import { ProjectState } from './state/project-state.js';
 
 export const globalProjectState = ProjectState.getInstance();
 
 // business logic
-const projectInput = new ProjectInput();
-const activePrjList = new ProjectList('active');
-const finishedPrjList = new ProjectList('finished');
+const projectInput = new Proj.ProjectInput();
+const activePrjList = new ProjList('active');
+const finishedPrjList = new ProjList('finished');
--- drag-and-drop/src/services/component.ts
+++ drag-and-drop/src/services/component.ts
@@ -1,4 +1,8 @@
-export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+// default - main export;
+// this allows importing in other files without {}
+// import Cmp from './...' | Cmp - alias
+
+export /*default*/ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement; // the hidden template element
   hostElement: T; // the output element
   element: U; // the element that we take from the hidden template and display in the output element
